b50d05d157604c0cb777e040b96cc3223b7f92ef
kskanak <kskanak26@gmail.com>
2022-11-08 23:42:09 +0600

    review post api created

--- index.js
+++ index.js
@@ -53,6 +53,11 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
   } finally {
   }
 }
